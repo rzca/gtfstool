88bdd4b9bba4efaa2079e158c9fb34db43f61fb8
rzca <142755153+rzca@users.noreply.github.com>
2024-11-30 21:46:35 -0500

    implement file upload on front-end

--- gtfs-tool-app/src/App.tsx
+++ gtfs-tool-app/src/App.tsx
@@ -1,10 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import * as gtfs from "gtfs-tool";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [kml, setKml] = useState<string | undefined>(undefined)
+  const [buf, setBuf] = useState<ArrayBuffer | undefined>(undefined);
+;
+  useEffect(() => {
+    async function getKml() {
+      if (buf != undefined) {
+        const kml = await gtfs.toKml(buf);
+        console.log(kml);
+        setKml(kml);
+      }
+    }
+
+    getKml()
+  }, [buf]);
+
+  const handleFileUpload = async (files: FileList) => {
+
+    if (files[0] != null) {
+
+      const file = files[0];
+      const buffer = await file.arrayBuffer();
+      setBuf(buffer);
+    }
+
+  }
+
+  // const readFile = ()
 
   return (
     <>
@@ -24,6 +52,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <input type="file" onChange={e => e.target.files != null ? handleFileUpload(e.target.files) : undefined} />
+        <div>kml: {kml}</div>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
